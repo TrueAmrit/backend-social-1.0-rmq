a0e142a5f4b8e9a1c1e3a08cf3ca32c91c5ce7b4
Amrit Gupta <amritgupta1018@gmail.com>
2023-01-19 21:39:54 +0530

    updated

--- src/enums/platformEvents.ts
+++ src/enums/platformEvents.ts
@@ -3,4 +3,7 @@ export enum PlatformEvents {
   CONTENT_UPDATE = 'content_update_request',
   GROUP_CREATION = 'group_creation_request',
   GROUP_UPDATE = 'group_update_request',
+  REACTION_CREATION = 'reaction_creation_request',
+  REACTION_UPDATE = 'reaction_update_request',
+  REACTION_DELETE = 'reaction_delete_request',
 }
--- src/enums/rmqTopics.ts
+++ src/enums/rmqTopics.ts
@@ -3,4 +3,7 @@ export enum RmqTopics {
   CONTENT_UPDATE_TOPIC = 'content_update',
   GROUP_CREATION_TOPIC = 'group-creation',
   GROUP_UPDATE_TOPIC = 'group_update',
+  REACTION_CREATION_TOPIC = 'reaction_creation',
+  REACTION_UPDATE_TOPIC = 'reaction_update',
+  REACTION_DELETE_TOPIC = 'reaction_delete',
 }
